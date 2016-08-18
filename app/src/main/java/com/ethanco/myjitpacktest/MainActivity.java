ce59907a7b557cd03535f20138685d62ff4ce19e
EthanCo <fenghenyu@163.com>
2016-08-18 13:10:02 +0800

    add util

--- app/src/main/java/com/ethanco/myjitpacktest/MainActivity.java
+++ app/src/main/java/com/ethanco/myjitpacktest/MainActivity.java
@@ -3,11 +3,15 @@ package com.ethanco.myjitpacktest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ethanco.ethancoutil.EUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EUtil.toast(this, "JitPack");
     }
 }
---
+++ ethancoutil/src/main/java/com/ethanco/ethancoutil/EUtil.java
@@ -0,0 +1,13 @@
+package com.ethanco.ethancoutil;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by EthanCo on 2016/8/18.
+ */
+public class EUtil {
+    public static void toast(Context context, String msg) {
+        Toast.makeText(context, "=hhh= " + msg, Toast.LENGTH_SHORT).show();
+    }
+}
